1b1802c1efaf24e89d5e9c5a68f500c85a39d536
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-10-02 12:33:02 +0200

    added dynamic list, radio box

--- backEnd/index.js
+++ backEnd/index.js
@@ -11,6 +11,14 @@ const professions = [
     "Firefighter"
 ];
 
+console.table(professions)
+
+let txt = ""
+    for(let x of professions.keys()){
+        txt += "<input type=\"radio\" name=\"professionName\" value=\"" + professions[x] + "\">\n<label>" + professions[x] + "</label><br>"
+    }
+    document.getElementById("branch").innerHTML = "<form>\n" + txt + "\n</form>"
+
 function user(name, age, gender, occup, prof, email, pass, ){
     this.name = name;
     this.age = age;
@@ -22,9 +30,5 @@ function user(name, age, gender, occup, prof, email, pass, ){
 }
 
 function submit(){
-    let txt = ""
-    for(let x of professions.keys()){
-        txt += "<input type=\"checkbox\" value=\"" + professions[x] + "\">\n<label>" + professions[x] + "</label><br>"
-    }
-    document.getElementById("prof").innerHTML = "<form>\n" + txt + "\n</form>"
+    
 }
