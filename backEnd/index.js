7db3bac9cd5fa7aa44796583b05006f6d24c8281
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-10-02 11:17:22 +0200

    added array and class

---
+++ backEnd/index.js
@@ -0,0 +1,20 @@
+const professions = [
+    "Cashier",
+    "Policeman",
+    "Pilot",
+    "Soccer player",
+    "Teacher",
+    "Private investigator",
+    "Atronaut",
+    "Hairdresser",
+    "Magician",
+    "Firefighter"
+];
+
+function user(name, age, occup, prof, ){
+
+}
+
+function submit(){
+
+}
