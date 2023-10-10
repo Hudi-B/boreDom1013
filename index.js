ed6d791735c34f6188bfba5c3d78ef4ce508a368
RRichard2004 <ricsiruszo90@gmail.com>
2023-10-10 09:10:52 +0200

    Dropdown menu
    inserted and corrected a dropdown menu, still unfinished, no correct values in it

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+
+
+
 jQuery('.button_gender').click(function(){
     var button = jQuery(this);
     if (button.hasClass('active') == false){
@@ -62,19 +65,6 @@ function user(name, age, gender, occup, prof, email, pass, ){
     this.pass = pass;
 }
 
-
 /*Dropdown Menu*/
-$('.dropdown').click(function () {
-  $(this).attr('tabindex', 1).focus();
-  $(this).toggleClass('active');
-  $(this).find('.dropdown-menu').slideToggle(300);
-});
-$('.dropdown').focusout(function () {
-  $(this).removeClass('active');
-  $(this).find('.dropdown-menu').slideUp(300);
-});
-$('.dropdown .dropdown-menu li').click(function () {
-  $(this).parents('.dropdown').find('span').text($(this).text());
-  $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-});
+
 /*End Dropdown Menu*/
