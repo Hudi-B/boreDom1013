e222820849625f7e9baed598bda8fe852c42adc5
Hudi-B <vanosskanoss@gmail.com>
2023-10-11 12:24:01 +0200

    added icons and edited gender radio

---
+++ index.js
@@ -0,0 +1,6 @@
+var isOpen = 0; //Fix this!!!!!!!!!!!!!
+
+function Dropdown() {
+    let x = document.getElementById('dropIcon')
+    x.innerHTML = isOpen ? "arrow_drop_down" : "arrow_drop_down";
+} //!!!!!!!!!!!!!
