3bd49322fc9aefafd97736e8486911793a544c45
Hudi-B <vanosskanoss@gmail.com>
2023-10-13 00:18:59 +0200

    added form validation
    validated inputs
    created output alerts(sweetalert2)
    created output string on console

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function preventSubmit(event) {
+    event.preventDefault();
+}
+
 var ageRange = document.getElementById('ageRange');
 var ageShow = document.getElementById('ageShow');
 var genderShow = document.getElementById('genderShow');
@@ -7,7 +11,12 @@ var passwordInputRe = document.getElementById('passwordInputRe');
 var passVisIcon = document.getElementById('passVisIcon');
 var passGenIcon = document.getElementById('passGenIcon');
 var jobSelect = document.getElementById('jobSelect');
+var selectedGenderOut = ""
 
+function selectedGender(selectedGender) {
+    genderShow.textContent = selectedGender.checked ? selectedGender.value : "Select a gender";
+    selectedGenderOut = selectedGender.value
+}
 var fields = [
     "Egészségügy",
     "Élelmiszeripar",
@@ -19,13 +28,13 @@ var fields = [
     "Informatika és távközlés",
     "Honvédelem",
     "Kereskedelem",
-    "Közlekedés és szállítmányozás"
+    "Közlekedés és szállítmányozás",
 ]
 
 fields.forEach(function(element) {
     const option = document.createElement("option")
     option.text = element;
-    option.style = "color: #383838;";
+    option.style.color = "#383838";
 
         jobSelect.appendChild(option);
 });
@@ -33,12 +42,6 @@ fields.forEach(function(element) {
 ageRange.addEventListener("input", function(){
     ageShow.textContent = ageRange.value;
 })
-
-
-
-function selectedGender(selectedGender) {
-    genderShow.textContent = selectedGender.checked ? selectedGender.value : "Select a gender";
-}
 
 passVisIcon.addEventListener("click", function() {
     if (passwordInput.type == "text") {
@@ -82,6 +85,94 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
 
-function test() {
-    alert('button pressed')
+function areFirstLettersUppercase(inputString) {
+    return inputString.split(' ').every(word => word[0] === word[0].toUpperCase());
+}
+
+function doesNotIncludeNumber(inputString) {
+const noNumberPattern = /^[^0-9]*$/;
+return noNumberPattern.test(inputString);
+}
+  
+
+function isValidEmail(email) {
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+return emailPattern.test(email);
+}
+
+function onSubmit() {
+    let nameInput = document.getElementById('nameInput')
+    let emailInput = document.getElementById('emailInput')
+    let passwordInput = document.getElementById('passwordInput')
+    let passwordInputRe = document.getElementById('passwordInputRe')
+    let ageRange = document.getElementById('ageRange')
+    let ageShow = document.getElementById('ageShow')
+    let maleRadio = document.getElementById('maleRadio')
+    let femaleRadio = document.getElementById('femaleRadio')
+    let otherRadio = document.getElementById('otherRadio')
+    let fieldInput = document.getElementById('fieldInput')
+    let jobSelect = document.getElementById('jobSelect')
+    let succesfullValidation = true;
+
+    let errorsArray = []
+
+    if (nameInput.checkValidity() && emailInput.checkValidity() && passwordInput.checkValidity() && fieldInput.checkValidity() && jobSelect.checkValidity()) {
+        if (!areFirstLettersUppercase(nameInput.value)) {
+            succesfullValidation = false;
+            errorsArray.push("A nevek első betűi nagyok lehetnek csak.")
+        }
+        if (!doesNotIncludeNumber(nameInput.value)) {
+            succesfullValidation = false;
+            errorsArray.push("A név mező nem tartalmazhat számot")
+        }
+        if (!isValidEmail(emailInput.value)) {
+            succesfullValidation = false;
+            errorsArray.push("A email mező nem egyezik meg a sablonnak[pelda@pelda.pelda]")
+        }
+        if (passwordInput.value != passwordInputRe.value) {
+            succesfullValidation = false;
+            errorsArray.push("A jelszavak nem egyeznek meg.")
+        }
+        if (isNaN(ageShow.textContent)) {
+            succesfullValidation = false;
+            errorsArray.push("Nem valasztott kort.")
+        }
+        if (!maleRadio.checked && !femaleRadio.checked && !otherRadio.checked) {
+            succesfullValidation = false;
+            errorsArray.push("Nem valasztott nemet.")
+        }
+    }
+    else succesfullValidation = false;
+
+    if (succesfullValidation) {
+        Swal.fire({
+            title: 'Siker!',
+            text: 'Sikeresen regisztált.',
+            icon: 'success',
+            confirmButtonText: 'Rendben',
+            confirmButtonColor: 'greenyellow',
+            color: 'white',
+            iconColor: 'greenyellow',
+            background: '#383838',
+          })
+        console.log(`Név:${nameInput.value} Email:${emailInput.value} Password:${passwordInput.value} Age:${ageRange.value} Gender:${selectedGenderOut} Field:${fieldInput.value} jobType:${jobSelect.value}`);
+    }
+    else if (!succesfullValidation && errorsArray.length > 0) {
+        let errors = ""
+        errorsArray.forEach(function(element) {
+            errors += element + "<br>"
+        });
+
+        Swal.fire({
+            title: 'Hiba!',
+            text: 'Valami hiba merült fel.',
+            icon: 'error',
+            confirmButtonText: 'Újra',
+            confirmButtonColor: 'red',
+            color: '#fff',
+            iconColor: 'red',
+            background: '#383838',
+            footer: errors
+          })
+    }
 }
